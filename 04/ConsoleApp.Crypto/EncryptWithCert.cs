0ed67248b3cf4a6fcee0a7d8bd6ec3f1fba0a5b5
rmauro <ricardo.demauro@gmail.com>
2019-10-09 18:03:01 -0300

    adding rsa key

---
+++ 04/ConsoleApp.Crypto/EncryptWithCert.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ConsoleApp.Crypto
+{
+    public class EncryptWithCert
+    {
+        public void Run()
+        {
+            string plainText = "Test data to be encrypted";
+            string encryptedText = string.Empty;
+
+            // Get the certifcate to use to encrypt the key. 
+            X509Certificate2 cert = GetCertificateFromStore("CN=CERT_SIGN_TEST_CERT");
+            if (cert == null)
+                throw new Exception("Certificate 'CN=CERT_SIGN_TEST_CERT' not found.");
+
+
+            RSA publicKey = cert.GetRSAPublicKey();
+            // Encrypt the file using the public key from the certificate. 
+            encryptedText = EncryptFile(plainText, publicKey);
+
+            RSA privateKey = cert.GetRSAPrivateKey();
+            // Decrypt the file using the private key from the certificate. 
+            string decryptedText = DecryptFile(encryptedText, privateKey);
+
+            //Display the original data and the decrypted data. 
+            Console.WriteLine("Original:   {0}", plainText);
+            Console.WriteLine("Encrypted:  {0}", encryptedText);
+            Console.WriteLine("Decrypted:  {0}", decryptedText);
+        }
+
+        private static X509Certificate2 GetCertificateFromStore(string certName)
+        {
+            // Get the certificate store for the current user. 
+            X509Store store = new X509Store(StoreLocation.CurrentUser);
+            try
+            {
+                store.Open(OpenFlags.ReadOnly);
+
+                X509Certificate2Collection certCollection = store.Certificates;
+
+                X509Certificate2Collection currentCerts = certCollection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
+
+                X509Certificate2Collection signingCert = currentCerts.Find(X509FindType.FindBySubjectDistinguishedName, certName, false);
+
+                if (signingCert.Count == 0)
+                    return null;
+
+                // Return the first certificate in the collection, has the right name and is current. 
+                return signingCert[0];
+            }
+            finally
+            {
+                store.Close();
+            }
+        }
+
+        // Encrypt a file using a public key. 
+        private string EncryptFile(string stringToEncrypt, RSA rsaPublicKey)
+        {
+            if (rsaPublicKey == null || string.IsNullOrEmpty(stringToEncrypt))
+                throw new Exception("A x509 certificate and string for encryption must be provided");
+
+            byte[] bytestoEncrypt = Encoding.ASCII.GetBytes(stringToEncrypt);
+            byte[] encryptedBytes = rsaPublicKey.Encrypt(bytestoEncrypt, RSAEncryptionPadding.OaepSHA1);
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+        // Decrypt a file using a private key. 
+        private string DecryptFile(string stringToDecrypt, RSA rsaPrivateKey)
+        {
+            if (rsaPrivateKey == null || string.IsNullOrEmpty(stringToDecrypt))
+                throw new Exception("A x509 certificate and string for decryption must be provided");
+
+            byte[] bytestodecrypt = Convert.FromBase64String(stringToDecrypt);
+            byte[] plainbytes = rsaPrivateKey.Decrypt(bytestodecrypt, RSAEncryptionPadding.OaepSHA1);
+            ASCIIEncoding enc = new ASCIIEncoding();
+            return enc.GetString(plainbytes);
+        }
+    }
+}
---
+++ 04/ConsoleApp.Crypto/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ConsoleApp.Crypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string planText = "text to encryp";
+
+            EncryptWithCert encryptWithCert = new EncryptWithCert();
+            encryptWithCert.Run();
+
+            string @string = "Super string master blaster!!!!";
+            Console.WriteLine($"One way Hash MD5 {ComputeHashMD5(@string)}");
+
+            string encrypted = EncrypHMAC(planText, "HAHAHAHAHAHAHAHAHA");
+            Console.WriteLine($"EncrypHMAC {encrypted}");
+
+
+
+            //Create a UnicodeEncoder to convert between byte array and string.
+            UnicodeEncoding ByteConverter = new UnicodeEncoding();
+
+            //Create byte arrays to hold original, encrypted, and decrypted data.
+            byte[] dataToEncrypt = ByteConverter.GetBytes("Data to Encrypt");
+            byte[] encryptedData;
+            byte[] decryptedData;
+
+            //Create a new instance of RSACryptoServiceProvider to generate
+            //public and private key data.
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                //Pass the data to ENCRYPT, the public key information 
+                //(using RSACryptoServiceProvider.ExportParameters(false),
+                //and a boolean flag specifying no OAEP padding.
+                encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
+
+                //Pass the data to DECRYPT, the private key information 
+                //(using RSACryptoServiceProvider.ExportParameters(true),
+                //and a boolean flag specifying no OAEP padding.
+                decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
+
+                //Display the decrypted plaintext to the console. 
+                Console.WriteLine("Decrypted plaintext: {0}", ByteConverter.GetString(decryptedData));
+            }
+        }
+
+        static string ComputeHashMD5(string input)
+        {
+            using (var md5Hash = MD5.Create())
+            {
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                StringBuilder sBuilder = new StringBuilder();
+
+                for (int i = 0; i < data.Length; i++)
+                    sBuilder.Append(data[i].ToString("x2"));
+
+                return sBuilder.ToString();
+            }
+        }
+
+        static string EncrypHMAC(string input, string secretKey)
+        {
+            var rawInput = Encoding.UTF8.GetBytes(input);
+            var rawSecret = Encoding.UTF8.GetBytes(secretKey);
+
+            using (var hashAlgorithm = new HMACSHA1(rawSecret))
+            {
+                using (var bufferStream = new MemoryStream(rawInput))
+                {
+                    var bytesEncrypted = hashAlgorithm.ComputeHash(bufferStream);
+
+                    return Encoding.UTF8.GetString(bytesEncrypted);
+                }
+            }
+        }
+
+        public static byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    //Import the RSA Key information. This only needs
+                    //toinclude the public key information.
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    //Encrypt the passed byte array and specify OAEP padding.  
+                    //OAEP padding is only available on Microsoft Windows XP or
+                    //later.  
+                    encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            //Catch and display a CryptographicException  
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+        public static byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                //Create a new instance of RSACryptoServiceProvider.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    //Import the RSA Key information. This needs
+                    //to include the private key information.
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    //Decrypt the passed byte array and specify OAEP padding.  
+                    //OAEP padding is only available on Microsoft Windows XP or
+                    //later.  
+                    decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            //Catch and display a CryptographicException  
+            //to the console.
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+
+                return null;
+            }
+        }
+
+        static void ShowCerts()
+        {
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadOnly);
+
+            foreach (var item in store.Certificates)
+            {
+                Console.WriteLine($"Cert Subject {item.Subject} Thumbprint {item.Thumbprint}");
+            }
+
+            store.Close();
+        }
+
+
+        public static byte[] EncryptDataOaepSha1(X509Certificate2 cert, string input)
+        {
+            // GetRSAPublicKey returns an object with an independent lifetime, so it should be
+            // handled via a using statement.
+            using (RSA rsa = cert.GetRSAPublicKey())
+            {
+                byte[] dataBytes = Encoding.UTF8.GetBytes(input);
+
+                // OAEP allows for multiple hashing algorithms, what was formermly just "OAEP" is
+                // now OAEP-SHA1.
+                return rsa.Encrypt(dataBytes, RSAEncryptionPadding.OaepSHA1);
+            }
+        }
+
+        public static byte[] DecryptDataOaepSha1(X509Certificate2 cert, string input)
+        {
+            // GetRSAPrivateKey returns an object with an independent lifetime, so it should be
+            // handled via a using statement.
+            using (RSA rsa = cert.GetRSAPrivateKey())
+            {
+                byte[] dataBytes = Encoding.UTF8.GetBytes(input);
+
+                return rsa.Decrypt(dataBytes, RSAEncryptionPadding.OaepSHA1);
+            }
+        }
+    }
+}
