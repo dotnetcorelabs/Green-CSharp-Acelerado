1bde3476ebd57f4906735f95cc99223490c4c121
rmauro <ricardo.demauro@gmail.com>
2019-10-09 15:13:07 -0300

    adding reflection

---
+++ 04/ConsoleApp.Reflection/Contato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Reflection
+{
+    [Serializable]
+    public class Contato
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Telefone { get; set; }
+    }
+}
--- 04/ConsoleApp.Reflection/Program.cs
+++ 04/ConsoleApp.Reflection/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace ConsoleApp.Reflection
 {
@@ -6,7 +7,40 @@ namespace ConsoleApp.Reflection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! from reflection");
+
+            Contato c = new Contato();
+
+            c.Id = Guid.NewGuid();
+            c.Nome = "Nome teste";
+            c.Telefone = "11 929292929";
+
+            Type tContato = typeof(Contato);
+
+            Console.WriteLine($"Type Info - Comecando");
+            Console.WriteLine($"Type Info - {tContato.Name}");
+            Console.WriteLine($"Type Info - {tContato.Assembly.FullName}");
+            Console.WriteLine($"Type Info - {tContato.BaseType?.Name}");
+            Console.WriteLine($"Type Info - Fim");
+
+            var attributes = tContato.CustomAttributes;
+            foreach (var item in attributes)
+            {
+                Console.WriteLine($"Type Info - Attributes {item.AttributeType.Name}");
+            }
+
+            var publicProperties = tContato.GetProperties();
+            foreach (var item in publicProperties)
+            {
+                Console.WriteLine($"Type Info - Public Property Name {item.Name} Value {tContato.GetProperty(item.Name).GetValue(c)}");
+            }
+
+            var p = tContato.GetProperty("Nome");
+            p.SetValue(c, "Nome do Reflection");
+
+            Console.WriteLine($"Set Reflection - Property Nome --> {c.Nome}");
+
+            Console.ReadKey();
         }
     }
 }
